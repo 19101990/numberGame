b5b038d0823fce9d324f7246eec5f91c697ba864
19101990 <exo.gf.s@gmail.com>
2018-04-24 07:32:53 +0200

    add timeIt function

--- numberGame.py
+++ numberGame.py
@@ -2,15 +2,20 @@ from tkinter import *
 from tkinter import messagebox
 import random
 
-numbers = random.sample(range(0,1000),25)
 root = Tk()
 root.title("From Lowest to Highest")
+
+numbers = random.sample(range(0,1000),25)
 ind = 0
 errorList = []
 errors = StringVar()
+count = True
+time = StringVar()
+s = 0
+m = 0
 
 def findMin(i,n):
-    global ind, errorList
+    global ind, errorList, count
 
     numbers.sort()
     if int(numbers[ind]) == buttons[i].cget('text'):
@@ -20,7 +25,30 @@ def findMin(i,n):
         errorList.append(1)
         errors.set("Errors: " + str(len(errorList)))
     if ind == 25:
+        count = False
         messagebox.showinfo("Congratulation!", "Congratulation! You finished your task.")
+
+
+def timeIt(*args):
+    global s, m, count, time
+    if s < 10:
+        seconds = "0"+str(s)
+    else:
+        seconds = str(s)
+    if s > 59:
+        m += 1
+        s = 0
+        seconds = "0"+str(s)
+    if m < 10:
+        minutes = "0"+str(m)
+    else:
+        minutes = str(m)
+
+    time.set(str(minutes) + ":" + str(seconds))
+    if count == True:
+        root.after(1000, timeIt)
+        s += 1
+        
     
 buttons = []
 
@@ -31,7 +59,11 @@ for i in range(25):
     button.grid(row=i%5, column=i//5)
     buttons.append(button)
 
-ErrorCounter = Label(root, text="Errors: 0", textvariable=errors)
-ErrorCounter.grid(row=5, column=0)
+ErrorCounter = Label(root, textvariable=errors)
+ErrorCounter.grid(row=5, column=2)
+timer = Label(root, textvariable=time)
+timer.grid(row=5, column=0)
+
+timeIt()
 
 root.mainloop()
